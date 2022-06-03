39c4edb976b6f1a63cbfae89da1aef6f26a9264c
Nika V <76059172+nikavolk@users.noreply.github.com>
2022-06-03 12:40:16 +0200

    push theme files

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+/* function to set active to navbar link */
+const navBar = document.querySelectorAll("header nav ul.menu li a");
+for (let i = 0, length = navBar.length; i < length; i++) {
+  navBar[i].onclick = function () {
+    var findActive = document.querySelector(".active");
+    if (findActive) findActive.classList.remove("active");
+    this.parentNode.classList.add("active");
+  };
+}
+
+/* function for shrinking the header and setting its opacity 
+after user scrolls down by 120px */
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const selectHeader = document.querySelector("header").style;
+  if (
+    document.body.scrollTop > 120 ||
+    document.documentElement.scrollTop > 120
+  ) {
+    selectHeader.padding = "5px 10px";
+    selectHeader.backgroundColor = "rgba(0,0,0,1)";
+  } else {
+    selectHeader.padding = "25px 10px";
+    selectHeader.backgroundColor = "rgba(0,0,0,0.5)";
+  }
+}
+
+// add squares
+
+const squareSelector = document.querySelector("#challenge-about-me");
+squareSelector.insertAdjacentHTML("afterbegin", '<div class="square1"></div>');
+squareSelector.insertAdjacentHTML("afterbegin", '<div class="square2"></div>');
+
+// add span to slogan-title
+const sloganSelector = document.querySelector(".site-slogan");
+
+let wordSelector = "";
+wordSelector += "<span class='site-slogan-bold'>";
+wordSelector += sloganSelector.textContent.substring(0, 5);
+wordSelector += "</span>";
+wordSelector += sloganSelector.textContent.substring(5, 10);
+
+sloganSelector.innerHTML = wordSelector;
